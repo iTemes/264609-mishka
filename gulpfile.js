804e24d7ae521531a75ed23695792d665e260827
Artem Aksentenko <akstemes@gmail.com>
2017-04-03 19:20:22 +0300

    Добавлены mqpacker,csso,svgstore

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -15,11 +20,33 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer({browsers: ["last 2 versions"]}),
       mqpacker({
-        sort: true;
+        sort: true
       })
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("img"));
+})
+//
+gulp.task("symbols", function() {
+  return gulp.src("img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("img"));
 });
 
 gulp.task("serve", ["style"], function() {
