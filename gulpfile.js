4a8c09d0c67292c45eaa8cd4154bd3752f44a789
Artem Aksentenko <akstemes@gmail.com>
2017-04-03 17:36:57 +0300

    Начало автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,17 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var mqpacker = require("css-mqpacker");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 2 versions"
-      ]})
+      autoprefixer({browsers: ["last 2 versions"]}),
+      mqpacker({
+        sort: true;
+      })
     ]))
     .pipe(gulp.dest("css"))
     .pipe(server.stream());
